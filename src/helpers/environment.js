9a4cec89e4255c8903208483fb4623ac6f5b6283
Nav Loveday <twelvetreeorchard@gmail.com>
2020-10-13 11:51:48 -0400

    update client url in environment helpers

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000'
         break;
-    case 'https://campawesomesauce.herokuapp.com':
+    case 'campawesomesauce.herokuapp.com':
         APIURL = 'https://server-campawesomesauce.herokuapp.com'
 }
 export default APIURL;
